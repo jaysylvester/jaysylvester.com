a5e3e29d9b835436c5bc138ec0aa1b5c2cee1a0a
Jay Sylvester <jay@jaysylvester.com>
2018-10-28 19:08:36 -0400

    Contact form server side error handling

--- app/patterns/controllers/contact.js
+++ app/patterns/controllers/contact.js
@@ -23,27 +23,68 @@ function handler(params, context, emitter) {
 }
 
 function form(params, context, emitter) {
+  let emailRegex = new RegExp(/[a-z0-9!##$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!##$%&''*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i)
+
   if ( params.request.method !== 'POST' ) {
     emitter.emit('ready', { redirect: '/contact' })
   } else {
-    app.toolbox.mail.sendMail({
-      from: { name: params.form.name, address: params.form.email },
-      to: { name: 'Jay Sylvester', address: 'jay@jaysylvester.com' },
-      cc: params.form.cc ? { name: params.form.name, address: params.form.email } : {},
-      subject: params.form.subject + ' (sent via contact form)',
-      text: params.form.message
-    })
-
-    emitter.emit('ready', {
-      view: 'confirmation',
-      include: {
-        header: {
-          controller: '_header'
+    if ( !params.form.name.length || !params.form.email.length || !params.form.subject.length || !params.form.message.length ) {
+      emitter.emit('ready', {
+        content: {
+          error: 'All fields are required.'
         },
-        footer: {
-          controller: '_footer'
+        cache: {
+          route: false
+        },
+        include: {
+          header: {
+            controller: '_header'
+          },
+          footer: {
+            controller: '_footer'
+          }
         }
-      }
-    })
+      })
+    } else if ( !emailRegex.test(params.form.email) ) {
+      emitter.emit('ready', {
+        content: {
+          error: 'Your e-mail address doesn\'t look right.'
+        },
+        cache: {
+          route: false
+        },
+        include: {
+          header: {
+            controller: '_header'
+          },
+          footer: {
+            controller: '_footer'
+          }
+        }
+      })
+    } else {
+      app.toolbox.mail.sendMail({
+        from: { name: params.form.name, address: params.form.email },
+        to: { name: 'Jay Sylvester', address: 'jay@jaysylvester.com' },
+        cc: params.form.cc ? { name: params.form.name, address: params.form.email } : {},
+        subject: params.form.subject + ' (sent via contact form)',
+        text: params.form.message
+      })
+  
+      emitter.emit('ready', {
+        view: 'confirmation',
+        cache: {
+          route: false
+        },
+        include: {
+          header: {
+            controller: '_header'
+          },
+          footer: {
+            controller: '_footer'
+          }
+        }
+      })
+    }
   }
 }
