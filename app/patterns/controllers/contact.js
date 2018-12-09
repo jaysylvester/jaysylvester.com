e392fdab1fe23e3b73cf164b57603016c9165e4f
Jay Sylvester <jay@jaysylvester.com>
2018-12-09 05:39:31 -0500

    Spam prevention

--- app/patterns/controllers/contact.js
+++ app/patterns/controllers/contact.js
@@ -11,7 +11,11 @@ module.exports = {
 
 // default action
 function handler(params, context, emitter) {
-  emitter.emit('ready')
+  emitter.emit('ready', {
+    content: {
+      verification: verification()
+    }
+  })
 }
 
 function form(params, context, emitter) {
@@ -23,7 +27,8 @@ function form(params, context, emitter) {
     if ( !params.form.name.length || !params.form.email.length || !params.form.subject.length || !params.form.message.length ) {
       emitter.emit('ready', {
         content: {
-          error: 'All fields are required.'
+          error: 'All fields are required.',
+          verification: verification()
         },
         cache: {
           route: false
@@ -32,7 +37,18 @@ function form(params, context, emitter) {
     } else if ( !emailRegex.test(params.form.email) ) {
       emitter.emit('ready', {
         content: {
-          error: 'Your e-mail address doesn\'t look right.'
+          error: 'Your e-mail address doesn\'t look right.',
+          verification: verification()
+        },
+        cache: {
+          route: false
+        }
+      })
+    } else if ( parseInt(params.form.first_int, 10) + parseInt(params.form.second_int, 10) !== parseInt(params.form.verification, 10) ) {
+      emitter.emit('ready', {
+        content: {
+          error: 'Are you a bot? If not, please try again.',
+          verification: verification()
         },
         cache: {
           route: false
@@ -56,4 +72,24 @@ function form(params, context, emitter) {
 
 function confirmation(params, context, emitter) {
   emitter.emit('ready', { view: 'confirmation'} )
+}
+
+function verification() {
+  let verification = {}
+  verification.firstInt  = app.toolbox.helpers.getRandomIntInclusive(1, 10)
+  verification.secondInt = app.toolbox.helpers.getRandomIntInclusive(1, 10)
+  verification.answer    = verification.firstInt + verification.secondInt
+  verification.options   = []
+  for ( var i = 0; i < 5; i++ ) {
+    verification.options[app.toolbox.helpers.getRandomIntInclusive(0, 20)] = app.toolbox.helpers.getRandomIntInclusive(1, 20)
+  }
+  verification.options[app.toolbox.helpers.getRandomIntInclusive(0, 20)] = verification.answer
+  verification.options = verification.options.filter( function (element) {
+    return element != null
+  })
+  verification.options = verification.options.filter( function (item, index, array) {
+    return array.indexOf(item) === index
+  })
+  verification.options.sort((a, b) => a - b)
+  return verification
 }
--- app/start-local.js
+++ app/start-local.js
@@ -19,6 +19,7 @@ app.config.cacheBuster = {
 }
 
 app.toolbox = {
+  helpers: require('./toolbox/helpers'),
   // Third party modules
   mail: {
     sendMail: function (args) {
