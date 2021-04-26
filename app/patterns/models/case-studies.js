9977c54c538943c1aae6ea6b8a5ff50bbf850337
Jay Sylvester <jay@jaysylvester.com>
2021-04-26 04:56:38 -0400

    Remove model caches since the global route cache makes them redundant

--- app/patterns/models/case-studies.js
+++ app/patterns/models/case-studies.js
@@ -9,69 +9,31 @@ module.exports = {
 
 
 async function caseStudies(count = false) {
-  // See if the case study is already cached
-  let cacheKey  = 'case-studies-' + count,
-      scope     = 'case-studies',
-      cached    = app.cache.get({ scope: scope, key: cacheKey })
+  const client = await app.toolbox.dbPool.connect()
 
-  // If it's cached, return the cache object
-  if ( cached ) {
-    return cached
-  // If it's not cached, retrieve it from the database and cache it
-  } else {
-    const client = await app.toolbox.dbPool.connect()
-
-    try {
-      const result = await client.query({
-        name: count ? 'case_studies_caseStudies_count_' + count : 'case_studies_caseStudies',
-        text: 'select id, company_name, company_url, title, tagline, vertical, platform, expertise, summary, sort from case_studies order by sort asc' + ( count ? ' limit ' + count : '' ) + ';'
-      })
-      // Cache the case study for future requests
-      if ( !app.cache.exists({ scope: scope, key: cacheKey }) ) {
-        app.cache.set({
-          key: cacheKey,
-          scope: scope,
-          value: result.rows
-        })
-      }
-      return result.rows
-    } finally {
-      client.release()
-    }
+  try {
+    const result = await client.query({
+      name: count ? 'case_studies_caseStudies_count_' + count : 'case_studies_caseStudies',
+      text: 'select id, company_name, company_url, title, tagline, vertical, platform, expertise, summary, sort from case_studies order by sort asc' + ( count ? ' limit ' + count : '' ) + ';'
+    })
+    return result.rows
+  } finally {
+    client.release()
   }
 }
 
 
 async function caseStudy(companyUrl) {
-  // See if the case study is already cached
-  let cacheKey  = 'case-study-' + companyUrl,
-      scope     = 'case-studies',
-      cached    = app.cache.get({ scope: scope, key: cacheKey })
+  const client = await app.toolbox.dbPool.connect()
 
-  // If it's cached, return the cache object
-  if ( cached ) {
-    return cached
-  // If it's not cached, retrieve it from the database and cache it
-  } else {
-    const client = await app.toolbox.dbPool.connect()
-
-    try {
-      const result = await client.query({
-        name: 'case_studies_caseStudy',
-        text: 'select id, company_name, company_url, title, tagline, vertical, platform, expertise, summary, content from case_studies where company_url = $1;',
-        values: [ companyUrl ]
-      })
-      // Cache the case study for future requests
-      if ( !app.cache.exists({ scope: scope, key: cacheKey }) ) {
-        app.cache.set({
-          key: cacheKey,
-          scope: scope,
-          value: result.rows[0]
-        })
-      }
-      return result.rows[0]
-    } finally {
-      client.release()
-    }
+  try {
+    const result = await client.query({
+      name: 'case_studies_caseStudy',
+      text: 'select id, company_name, company_url, title, tagline, vertical, platform, expertise, summary, content from case_studies where company_url = $1;',
+      values: [ companyUrl ]
+    })
+    return result.rows[0]
+  } finally {
+    client.release()
   }
 }
--- app/patterns/models/screens.js
+++ app/patterns/models/screens.js
@@ -9,105 +9,67 @@ module.exports = {
 
 
 async function companyScreens(company) {
-  // See if these screens are already cached
-  let cacheKey  = 'screens-' + company,
-      scope     = 'screens',
-      cached    = app.cache.get({ scope: scope, key: cacheKey })
-  
-  // If it's cached, return the cache object
-  if ( cached ) {
-    return cached
-  // If it's not cached, retrieve it from the database and cache it
-  } else {
-    const client = await app.toolbox.dbPool.connect()
+  const client = await app.toolbox.dbPool.connect()
 
-    try {
-      const result = await client.query({
-        name: 'case_study_screens',
-        text: 'select id, company, url, alt, category, sort from screens where company = $1 order by sort asc;',
-        values: [ company ]
-      })
-      // Transform the data to make it usable by the view
-      let screens = {}
-      result.rows.forEach( function (screen) {
-        if ( !screens[screen.category] ) {
-          screens[screen.category] = {
-            company: screen.company,
-            category: screen.category,
-            screens: {}
-          }
+  try {
+    const result = await client.query({
+      name: 'case_study_screens',
+      text: 'select id, company, url, alt, category, sort from screens where company = $1 order by sort asc;',
+      values: [ company ]
+    })
+    // Transform the data to make it usable by the view
+    let screens = {}
+    result.rows.forEach( function (screen) {
+      if ( !screens[screen.category] ) {
+        screens[screen.category] = {
+          company: screen.company,
+          category: screen.category,
+          screens: {}
         }
-        screens[screen.category].screens[screen.id] = {
-          company:  screen.company,
-          url:      screen.url,
-          alt:      screen.alt,
-          sort:     screen.sort,
-          category: screen.category
-        }
-      })
-      // Cache the case study for future requests
-      if ( !app.cache.exists({ scope: scope, key: cacheKey }) ) {
-        app.cache.set({
-          key: cacheKey,
-          scope: scope,
-          value: screens
-        })
       }
-      return screens
-    } finally {
-      client.release()
-    }
+      screens[screen.category].screens[screen.id] = {
+        company:  screen.company,
+        url:      screen.url,
+        alt:      screen.alt,
+        sort:     screen.sort,
+        category: screen.category
+      }
+    })
+    return screens
+  } finally {
+    client.release()
   }
 }
 
 
 async function screens() {
-  // See if these screens are already cached
-  let cacheKey  = 'all-screens',
-      scope     = 'screens',
-      cached    = app.cache.get({ scope: scope, key: cacheKey })
-  
-  // If it's cached, return the cache object
-  if ( cached ) {
-    return cached
-  // If it's not cached, retrieve it from the database and cache it
-  } else {
-    const client = await app.toolbox.dbPool.connect()
+  const client = await app.toolbox.dbPool.connect()
 
-    try {
-      const result = await client.query({
-        name: 'portfolio_screens',
-        text: 'select id, company, url, alt, category, sort from screens order by sort asc;'
-      })
-      // Transform the data to make it usable by the view
-      let screens = {}
-      result.rows.forEach( function (screen) {
-        if ( !screens[screen.category] ) {
-          screens[screen.category] = {
-            company: screen.company,
-            category: screen.category,
-            screens: {}
-          }
+  try {
+    const result = await client.query({
+      name: 'portfolio_screens',
+      text: 'select id, company, url, alt, category, sort from screens order by sort asc;'
+    })
+    // Transform the data to make it usable by the view
+    let screens = {}
+    result.rows.forEach( function (screen) {
+      if ( !screens[screen.category] ) {
+        screens[screen.category] = {
+          company: screen.company,
+          category: screen.category,
+          screens: {}
         }
-        screens[screen.category].screens[screen.id] = {
-          company:  screen.company,
-          url:      screen.url,
-          alt:      screen.alt,
-          sort:     screen.sort,
-          category: screen.category
-        }
-      })
-      // Cache the case study for future requests
-      if ( !app.cache.exists({ scope: scope, key: cacheKey }) ) {
-        app.cache.set({
-          key: cacheKey,
-          scope: scope,
-          value: screens
-        })
       }
-      return screens
-    } finally {
-      client.release()
-    }
+      screens[screen.category].screens[screen.id] = {
+        company:  screen.company,
+        url:      screen.url,
+        alt:      screen.alt,
+        sort:     screen.sort,
+        category: screen.category
+      }
+    })
+    return screens
+  } finally {
+    client.release()
   }
 }
