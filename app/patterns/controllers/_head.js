f8bf2a4a780a635961a014d0b3a3d9ab8466022a
Jay Sylvester <jay@jaysylvester.com>
2021-06-01 20:40:41 -0400

    Latest citizen 1.0.0 beta

--- app/patterns/controllers/_head.js
+++ app/patterns/controllers/_head.js
@@ -8,9 +8,19 @@ module.exports = {
 
 // default action
 async function handler(params) {
+  let metaData = {}
+  if ( app.models._head.default[params.route.controller] ) {
+    metaData = await app.models._head.default[params.route.controller](params.url.company)
+  } else {
+    metaData = {
+      title: 'Error',
+      description: 'This request generated an error.',
+      keywords: ''
+    }
+  }
   return {
     content: {
-      metaData: await app.models._head.default[params.route.controller](params.url.company),
+      metaData: metaData,
       tracking: app.config.citizen.mode === 'production' ? true : false
     }
   }
