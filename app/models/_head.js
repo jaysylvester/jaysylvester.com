1c73ebd4dd398ab52467f07960d90aeee2f66318
Jay Sylvester <jay@jaysylvester.com>
2024-11-26 00:47:16 -0500

    Metadata

--- app/models/_head.js
+++ app/models/_head.js
@@ -5,7 +5,7 @@ export const caseStudies = () => {
   return {
     title:        'Jay Sylvester — Case Studies',
     description:  'Read detailed case studies that describe my contribution to various UX and development projects.',
-    keywords:     'UI, UX, user experience, front end development, case studies'
+    keywords:     'UI, UX, user experience, product design, front end development, case studies'
   }
 }
 
@@ -17,7 +17,7 @@ export const caseStudy = async (company) => {
     return {
       title:        'Jay Sylvester — Case Study: ' + caseStudy.title,
       description:  caseStudy.summary,
-      keywords:     'UI, UX, user experience, UX case study, ' + caseStudy.company_name + ' case study'
+      keywords:     'UI, UX, user experience, product design, UX case study, ' + caseStudy.company_name + ' case study'
     }
   } else {
     return false
@@ -36,7 +36,7 @@ export const contact = () => {
 
 export const index = () => {
   return {
-    title:       'Jay Sylvester — UX Manager/Director, Designer, and Engineer',
+    title:       'Jay Sylvester — UX Manager/Director, Product Designer, and Engineer',
     description: 'I\'m a UX director/lead with almost 20 years experience spanning user research, user experience design, and full stack development.',
     keywords:    'UX, UI, user experience, ux manager, ux lead, ux director, ux researcher, ux consultant'
   }
@@ -45,9 +45,9 @@ export const index = () => {
 
 export const portfolio = () => {
   return {
-    title:       'Jay Sylvester\'s Portfolio — UX Designer and Engineer',
+    title:       'Jay Sylvester\'s Portfolio — UX Manager/Director, Product Designer, and Engineer',
     description: 'Enjoy my dry process documentation, research data, ugly sketches, plain-looking wireframes, and half-baked prototypes. And a few pretty screens for those of you who are into that.',
-    keywords:    'UI, UX, ux designer portfolio'
+    keywords:    'UI, UX, ux designer portfolio, product designer portfolio'
   }
 }
 
@@ -56,6 +56,6 @@ export const resume = () => {
   return {
     title:       'Jay Sylvester — Résumé / Work History',
     description: 'I\'ve led and contributed to just about every phase of the development lifecycle, including team management, information architecture, UX design, and full stack development.',
-    keywords:    'UI, UX, ux designer resume, ux designer cv, ux manager, ux director, front end developer, ui engineer'
+    keywords:    'UI, UX, ux designer resume, ux designer cv, ux manager, ux director, product designer, front end developer, ui engineer'
   }
 }
